f5f4b8f03fa841c82540d97683d34e6d9f6509a0
Quang Tri <hangquangtri3111996@gmail.com>
2017-12-20 20:12:07 +0700

    Đăng ký lich làm việc (nhân viên)

--- NamelessWeb/Controllers/DangKyLichLamViecController.cs
+++ NamelessWeb/Controllers/DangKyLichLamViecController.cs
@@ -2,6 +2,7 @@
 using NamelessWeb.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,13 +11,22 @@ namespace NamelessWeb.Controllers
 {
     public class DangKyLichLamViecController : Controller
     {
+
         private readonly ApplicationDbContext dbContext;
+        SqlConnection connect = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NamelessWeb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         public DangKyLichLamViecController() {
-            dbContext = new ApplicationDbContext();
+            dbContext = new ApplicationDbContext();          
         }
         // GET: DangKyLichLamViec
+        [Authorize]
         public ActionResult DangKyLichLamViec()
         {
+            var userId = User.Identity.GetUserId();
+    
+            if (dbContext.DangKyLichLamViec.Count(l=>l.idUser==userId)>0)
+            {
+                return RedirectToAction("CapNhatLichLamViec", "DangKyLichLamViec");
+            }
             return View(new LichLamViecModel { heading="add schedule"});
         }
         [HttpPost]
@@ -34,7 +44,8 @@ namespace NamelessWeb.Controllers
         //Cập nhật lịch làm việc
         [Authorize]
         public ActionResult CapNhatLichLamViec() {
-            LichLamViecModel lichLamViec = dbContext.DangKyLichLamViec.Single(c => c.idUser.CompareTo(User.Identity.GetUserId())==0);
+            string userId = User.Identity.GetUserId();
+            LichLamViecModel lichLamViec = dbContext.DangKyLichLamViec.Single(c => c.idUser.CompareTo(userId)==0);
             lichLamViec.heading = "edit schedule";
             return View("DangKyLichLamViec",lichLamViec);
         }
@@ -43,9 +54,14 @@ namespace NamelessWeb.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult CapNhatLichLamViec(LichLamViecModel lichLamViec)
         {
-            var lichtemp = dbContext.DangKyLichLamViec.Single(c => c.idLich.CompareTo(lichLamViec.idLich)==0);
-            lichtemp = lichLamViec;
-            dbContext.SaveChanges();
+            
+            string userId = lichLamViec.idUser;
+            //var lichtemp = dbContext.DangKyLichLamViec.Single(c => c.idUser.CompareTo(userId)==0);
+            //lichtemp = lichLamViec;
+            connect.Open();
+            string stringSQL = string.Format("update dbo.LichLamViecModels set sang2='{0}',sang3='{1}',sang4='{2}',sang5='{3}',sang6='{4}',sang7='{5}',sangCN='{6}',chieu2='{7}',chieu3='{8}',chieu4='{9}',chieu5='{10}',chieu6='{11}',chieu7='{12}',chieuCN='{13}' where idLich='{14}'", lichLamViec.sang2, lichLamViec.sang3, lichLamViec.sang4, lichLamViec.sang5, lichLamViec.sang6, lichLamViec.sang7, lichLamViec.sangCN, lichLamViec.chieu2, lichLamViec.chieu3, lichLamViec.chieu4, lichLamViec.chieu5, lichLamViec.chieu6, lichLamViec.chieu7, lichLamViec.chieuCN,lichLamViec.idLich);
+            SqlCommand commandSQL = new SqlCommand(stringSQL,connect);
+            commandSQL.ExecuteNonQuery();
             return RedirectToAction("Index", "Home");
         }
       
